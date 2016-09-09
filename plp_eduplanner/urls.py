c35ca3594bf0155489885f24f560d40bf5bad64d
Alexander Burtsev <ab@bramabrama.com>
2016-09-09 12:40:58 +0300

    fix login requirements for profession pages

--- plp_eduplanner/urls.py
+++ plp_eduplanner/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.decorators import login_required
 from plp_eduplanner import views
 
 urlpatterns = [
-    url('^professions/$', login_required(views.Professions.as_view()), name='professions'),
-    url('^profession/(?P<pk>\d+)/$', login_required(views.Profession.as_view()), name='profession'),
-    url('^profession/(?P<pk>\d+)/plan/$', login_required(views.ProfessionPlan.as_view()), name='profession_plan'),
+    #url('^professions/$', login_required(views.Professions.as_view()), name='professions'),
+    url('^profession/(?P<pk>\d+)/$', views.Profession.as_view(), name='profession'),
+    url('^profession/(?P<pk>\d+)/plan/$', views.ProfessionPlan.as_view(), name='profession_plan'),
     url('^profession/(?P<pk>\d+)/learn/$', login_required(views.LearnProfession.as_view()), name='learn_profession'),
     url('^plan/(?P<pk>\d+)/forget/$', login_required(views.ForgetPlan.as_view()), name='forget_plan'),
     url('^dashboard/$', login_required(views.Dashboard.as_view()), name='dashboard')
